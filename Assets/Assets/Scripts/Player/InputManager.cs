fdb8897d14be4d9a8a423c961de01c279e9d8568
Kishore M <148840060+Kishore-GameDev@users.noreply.github.com>
2026-05-24 22:24:21 +0530

    Implementing Player and working on Spline Mapping

--- Assets/Assets/Scripts/Player/InputManager.cs
+++ Assets/Assets/Scripts/Player/InputManager.cs
@@ -1,16 +1,34 @@
+using System;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private InputSystem_Actions playerInput;
+
+    private Vector2 moveInput;
+    public Action<Vector2> OnMoveContinues;
+
     void Start()
     {
-        
+        playerInput = new();
+
+        playerInput.Player.Move.performed += ctr => moveInput = ctr.ReadValue<Vector2>();
+        playerInput.Player.Move.canceled += ctr => moveInput = Vector2.zero;
+        playerInput.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveContinues();
+    }
+
+    private void MoveContinues()
+    {
+        OnMoveContinues?.Invoke(moveInput);
+    }
+
+    void OnDestroy()
+    {
+        playerInput.Disable();
     }
 }
--- Assets/Assets/Scripts/Player/PlayerController.cs
+++ Assets/Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,104 @@
+using Dreamteck.Splines;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private InputManager inputManager;
+    public SplineFollower follower;
+
+    [Header("Speed")]
+    public float maxSpeed = 70f;
+    public float acceleration = 40f;
+
+    [Header("Steering")]
+    public float steeringSpeed = 120f;
+    public float moveSideSpeed = 6f;
+    public float maxRoadOffset = 4f;
+
+    [Header("Height")]
+    public float heightOffset = 0.5f;
+
+    public Vector2 moveInput;
+
+    private float currentSpeed;
+    private float roadOffset;
+
+    // PLAYER'S OWN ROTATION
+    private float currentYaw;
+
     void Start()
     {
-        
+        inputManager.OnMoveContinues += MovementManagement;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MovementManagement(Vector2 moveInput)
     {
-        
+        //-----------------------------------
+        // INPUT
+        //-----------------------------------
+
+        float horizontal = moveInput.x;
+        float vertical = Mathf.Max(moveInput.y, 0f);
+
+        //-----------------------------------
+        // SPEED
+        //-----------------------------------
+
+        float targetSpeed = vertical * maxSpeed;
+
+        currentSpeed = Mathf.MoveTowards(
+            currentSpeed,
+            targetSpeed,
+            acceleration * Time.deltaTime
+        );
+
+        follower.followSpeed = currentSpeed;
+
+        //-----------------------------------
+        // ROAD OFFSET
+        //-----------------------------------
+
+        roadOffset +=
+            horizontal * moveSideSpeed * Time.deltaTime;
+
+        roadOffset = Mathf.Clamp(
+            roadOffset,
+            -maxRoadOffset,
+            maxRoadOffset
+        );
+
+        //-----------------------------------
+        // SPLINE SAMPLE
+        //-----------------------------------
+
+        SplineSample sample = follower.result;
+
+        //-----------------------------------
+        // POSITION
+        //-----------------------------------
+
+        Vector3 finalPos =
+            sample.position +
+            sample.right * roadOffset +
+            sample.up * heightOffset;
+
+        transform.position = finalPos;
+
+        //-----------------------------------
+        // MANUAL PLAYER ROTATION
+        //-----------------------------------
+
+        currentYaw +=
+            horizontal * steeringSpeed * Time.deltaTime;
+
+        Quaternion playerRotation =
+            Quaternion.Euler(0f, currentYaw, 0f);
+
+        transform.rotation = playerRotation;
+    }
+
+    void OnDestroy()
+    {
+        inputManager.OnMoveContinues -= MovementManagement;
     }
 }
